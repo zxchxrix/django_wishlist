fe6e5101120499fedc59aabc33b0efb7312299be
zacharia <zachariasalad>
2023-04-18 03:35:55 -0500

    css style changes

---
+++ travel_wishlist/functional_test.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from django.test import LiveServerTestCase
+
+
+class TitleTest(LiveServerTestCase):
+
+    selenium = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_title_on_home_page(self):
+        self.selenium.get(self.live_server_url)
+        self.assertIn('Travel Wishlist', self.selenium.title)
